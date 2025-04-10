754c84659ac23b624331cf0ef32b9125fbb95ea0
Renish Mandaviya <renishmandaviya@gmail.com>
2025-04-10 16:10:32 +0530

    update

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import vercel from 'vite-plugin-vercel';
 
 
 // https://vite.dev/config/
 export default defineConfig({
+  define: {
+    __APP_ENV__: process.env.VITE_VERCEL_ENV,
+  },
   plugins: [react(),
   tailwindcss(),
+  vercel(),
 ],
 })
